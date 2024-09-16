768dc9528b0fdd909bac717d5a1e01d6e2fe2d91
Sasank-V <sasanklearner@gmail.com>
2024-09-17 03:24:53 +0530

    Added All Task Page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,13 +3,14 @@ import Dashboard from "./pages/Dashboard.jsx"
 import Login from "./pages/Login.jsx"
 import NewTask from "./components/NewTask.jsx"
 import Signup from "./pages/Signup.jsx"
+import AllTasks from "./pages/AllTasks.jsx"
 
 const App = () => {
   const [pageNumber, setPageNumber] = useState(0)
 
   return (
     <div className="absolute w-full h-full">
-      <Dashboard pageNumber={pageNumber} setPageNumber={setPageNumber} />
+      <AllTasks/>
     </div>
   )
 }
