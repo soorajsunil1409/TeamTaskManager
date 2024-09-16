d5f42bb9e422c4cd67aae832e926c7527f36305d
Sooraj Sunil <sooraj.namboothiry@gmail.com>
2024-09-17 03:26:28 +0530

    Added Create task

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,13 +3,15 @@ import Dashboard from "./pages/Dashboard.jsx"
 import Login from "./pages/Login.jsx"
 import NewTask from "./components/NewTask.jsx"
 import Signup from "./pages/Signup.jsx"
+import TaskPopup from "./components/TaskPopup.jsx"
 
 const App = () => {
   const [pageNumber, setPageNumber] = useState(0)
 
   return (
     <div className="absolute w-full h-full">
-      <Dashboard pageNumber={pageNumber} setPageNumber={setPageNumber} />
+      {/* <Dashboard pageNumber={pageNumber} setPageNumber={setPageNumber} /> */}
+      <TaskPopup />
     </div>
   )
 }
