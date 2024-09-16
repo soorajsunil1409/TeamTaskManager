2a08f64b76342daec05053fa9c116d0fd3035046
Sooraj Sunil <sooraj.namboothiry@gmail.com>
2024-09-17 00:47:50 +0530

    Added Add task popup

--- Client/src/components/NewTask.jsx
+++ Client/src/components/NewTask.jsx
@@ -1,7 +1,11 @@
 function NewTask() {
+    const handleAddTask = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <>
-            <div className="w-full h-full flex justify-center bg-[#DEDEDE] items-center p-5 md:p-10">
+            <div className="w-full h-full flex justify-center bg-[#dedede00] items-center p-5 md:p-10">
                 <div className="w-full md:w-[70%] h-auto md:h-[650px] bg-black flex flex-col p-5 md:p-10 text-white shadow-[0_20px_50px_rgba(233,_117,_79,_0.7)] rounded-3xl">
                     <div className="h-auto text-2xl md:text-3xl font-bold mb-4 md:mb-5">
                         Create New Task
@@ -18,38 +22,38 @@ function NewTask() {
                         </div>
                         <div className="flex flex-col mb-4 md:mb-5">
                             <label className="text-white text-base md:text-xl">Title:</label>
-                            <input 
-                                name="task[title]" 
-                                placeholder="Enter the Title of the Task" 
+                            <input
+                                name="task[title]"
+                                placeholder="Enter the Title of the Task"
                                 className="py-2 px-3 md:py-2 md:px-4 mt-2 md:mt-3 rounded-full bg-white text-black text-sm md:text-base"
                             />
                         </div>
                         <div className="flex flex-col mb-4 md:mb-5">
                             <label className="text-white text-base md:text-xl">Description:</label>
-                            <textarea 
-                                name="task[desc]" 
+                            <textarea
+                                name="task[desc]"
                                 className="py-2 px-3 md:py-2 md:px-4 mt-2 md:mt-3 rounded-2xl bg-white text-black text-sm md:text-base"
                             />
                         </div>
                         <div className="flex flex-col md:flex-row">
                             <div className="flex flex-col mb-4 md:mb-5 w-full md:w-[40%] md:mr-5">
                                 <label className="text-white text-base md:text-xl">Deadline:</label>
-                                <input 
-                                    name="task[deadline]" 
-                                    type="datetime-local" 
+                                <input
+                                    name="task[deadline]"
+                                    type="datetime-local"
                                     className="py-2 px-3 md:py-2 md:px-4 mt-2 md:mt-3 rounded-2xl bg-white text-black text-sm md:text-base"
                                 />
                             </div>
                             <div className="flex flex-col mb-4 md:mb-5 w-full md:w-[55%]">
                                 <label className="text-white text-base md:text-xl">Comments:</label>
-                                <input 
-                                    name="task[comments]" 
+                                <input
+                                    name="task[comments]"
                                     className="py-2 px-3 md:py-2 md:px-4 mt-2 md:mt-3 rounded-2xl bg-white text-black text-sm md:text-base"
                                 />
                             </div>
                         </div>
                         <div className="flex justify-center mt-4 md:mt-5">
-                            <button className="bg-[#E9754F] py-2 px-4 md:py-3 md:px-5 rounded-full text-white text-sm md:text-base">Create</button>
+                            <button className="bg-[#E9754F] py-2 px-4 md:py-3 md:px-5 rounded-full text-white text-sm md:text-base" onClick={handleAddTask}>Create</button>
                         </div>
                     </form>
                 </div>
