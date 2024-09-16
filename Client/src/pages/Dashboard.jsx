f57e2660d5e18a889c70ab8e3cbaae146a3130a9
Sasank-V <sasanklearner@gmail.com>
2024-09-17 05:01:50 +0530

    Data put in DashBoard

--- Client/src/pages/Dashboard.jsx
+++ Client/src/pages/Dashboard.jsx
@@ -15,9 +15,8 @@ import { useNavigate } from 'react-router-dom';
 
 
 const Dashboard = () => {
-   localStorage.clear()
     const navigate=useNavigate();
-    const [pageNumber, setPageNumber] = useState(0)
+    const [pageNumber, setPageNumber] = useState(0);
     useEffect(() => {
         const isLoggedin = localStorage.getItem('isLoggedin') === 'true';
     
@@ -26,6 +25,45 @@ const Dashboard = () => {
           navigate('/Login');
         }
       }, [navigate]);
+
+      const [users, setUsers] = useState([]);
+      const [tasks,setTasks] = useState([]);
+      const [loading, setLoading] = useState(true);
+      const [error, setError] = useState(null);
+    
+      useEffect(() => {
+        // Fetch data from localhost:4000/api/user when the component mounts
+        fetch('http://localhost:4000/api/user/')
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            return response.json();
+          })
+          .then(data => {
+            setUsers(data); // Set the fetched data
+            setLoading(false);
+          })
+          .catch(error => {
+            setError(error); // Handle errors
+            setLoading(false);
+          });
+        fetch('http://localhost:4000/api/tasks/')
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            return response.json();
+          })
+          .then(data => {
+            setTasks(data); // Set the fetched data
+            setLoading(false);
+          })
+          .catch(error => {
+            setError(error); // Handle errors
+            setLoading(false);
+          });
+      }, []);
     return (
         <div className={`flex w-full h-full`}>
             <div className={`flex w-full h-full ${pageNumber == 1 ? 'blur-md' : ''} transition-all duration-150`}>
@@ -70,7 +108,7 @@ const Dashboard = () => {
                             <div className="border-2 rounded-lg md:w-[50%] h-full p-4">
                                 <div className="flex flex-col gap-4 justify-center h-full">
                                     <span className="text-[#777777]">Total Tasks</span>
-                                    <span className="text-3xl font-semibold">120</span>
+                                    <span className="text-3xl font-semibold">{tasks && tasks.length}</span>
                                     <span className="flex items-center gap-1 text-green-500">
                                         <BiSolidUpArrow />
                                         30%
@@ -80,7 +118,7 @@ const Dashboard = () => {
                             <div className="border-2 rounded-lg md:w-[50%] h-full p-4">
                                 <div className="flex flex-col gap-4 justify-center h-full">
                                     <span className="text-[#777777]">Total Team Members</span>
-                                    <span className="text-3xl font-semibold">10</span>
+                                    <span className="text-3xl font-semibold">{users && users.length}</span>
                                     <span className="flex items-center gap-1 text-red-500">
                                         <BiSolidDownArrow />
                                         10%
@@ -88,12 +126,56 @@ const Dashboard = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="w-full h-[50%] border-2 rounded-lg p-4">
-                            <div className="text-xl font-semibold flex justify-between items-center">
-                                Member Contributions
+                        <div className="w-full h-[60%] border-2 rounded-lg p-4">
+                            <div className="text-xl font-semibold flex justify-between items-center mb-5">
+                                <div className="mb-5">
+                                    Tasks Progress
+                                </div>
                                 <div className="absolute flex right-12 translate-y-2 cursor-pointer">
                                     <div className="px-5 py-2 rounded-lg border-2 border-[#FF906B] hover:bg-[#FF906B] hover:text-white">Filter</div>
                                 </div>
+                            </div>
+                            <div>
+                            
+                        {/* Wrap the table rows in a scrollable div */}
+                        <div className="w-full h-[300px] overflow-hid">
+                            <table className="w-full text-md sm:text-xl font-semibold bg-white rounded-3xl mb-4 border-separate border-spacing-0">
+                            <thead className="bg-black text-white">
+                                <tr>
+                                    <th className="p-3 border border-black">#</th>
+                                    <th className="px-10 border border-black">Task Name</th>
+                                    <th className="p-3 border border-black">Team Member</th>
+                                    <th className="p-3 border border-black">Status</th>
+                                    <th className="p-3 border border-black">Priority</th>
+                                </tr>
+                            </thead>
+                                <tbody>
+                                    <tr className="bg-white text-black">
+                                        <td className="p-3 text-center border border-black">1</td>
+                                        <td className="p-3 text-center border border-black">{tasks[0].title}</td>
+                                        <td className="p-3 text-center border border-black">{tasks[0].Member_id}</td>
+                                        <td className="p-3 text-center border border-black">{tasks[0].status}</td>
+                                        <td className="p-3 text-center border border-black">{tasks[0].priority}</td>
+                                    </tr>
+                                    <tr className="bg-white text-black">
+                                        <td className="p-3 text-center border border-black">2</td>
+                                        <td className="p-3 text-center border border-black">{tasks[1].title}</td>
+                                        <td className="p-3 text-center border border-black">{tasks[1].Member_id}</td>
+                                        <td className="p-3 text-center border border-black">{tasks[1].status}</td>
+                                        <td className="p-3 text-center border border-black">{tasks[1].priority}</td>
+                                    </tr>
+                                    <tr className="bg-white text-black">
+                                        <td className="p-3 text-center border border-black">3</td>
+                                        <td className="p-3 text-center border border-black">{tasks[2].title}</td>
+                                        <td className="p-3 text-center border border-black">{tasks[2].Member_id}</td>
+                                        <td className="p-3 text-center border border-black">{tasks[2].status}</td>
+                                        <td className="p-3 text-center border border-black">{tasks[2].priority}</td>
+                                    </tr>
+                                    
+                                    {/* Repeat for more rows */}
+                                </tbody>
+                            </table>
+                            </div>
                             </div>
                         </div>
                     </div>
