af644dbb2043336da87ce984720f4abedc0f03ee
legendtharun <manikandan211205@gmail.com>
2024-09-17 07:17:42 +0530

    nineth commit on Navigating

--- Client/src/components/Navmenu.jsx
+++ Client/src/components/Navmenu.jsx
@@ -50,6 +50,14 @@ const Navmenu = ({pageNumber,setPageNumber}) => {
                     <TiTick className="text-2xl" />
                     Tasks
                 </Link>
+                <Link to="/#" className={`p-3 flex items-center justify-start cursor-pointer gap-3 ${pageNumber == 2 ? 'bg-[#dedede]' : ''}`} onClick={() => setPageNumber(2)}>
+                    <IoPerson className="text-2xl" />
+                    Team
+                </Link>
+                <Link to="/#" className={`p-3 flex items-center justify-start cursor-pointer gap-3 ${pageNumber == 3 ? 'bg-[#dedede]' : ''}`} onClick={() => setPageNumber(3)}>
+                    <MdMenuBook className="text-2xl" />
+                    Overview
+                </Link>
                 
             </div>
         </motion.div>
