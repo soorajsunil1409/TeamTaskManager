84c8308a4a3154787d2384fc9defe5c533404eb4
Sooraj Sunil <sooraj.namboothiry@gmail.com>
2024-09-17 06:22:31 +0530

    Final Commit

--- Client/src/pages/Signup.jsx
+++ Client/src/pages/Signup.jsx
@@ -1,31 +1,32 @@
-import { Link, useNavigate} from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { useState } from "react"
 const Signup = () => {
-    const [UserName,setUserName]=useState("")
-const [Password,setPassword]=useState("")
-const [error,seterror]=useState("")
-const [Email,setEmail]=useState("")
-const [Role,setRole]=useState("Team Lead")
-const navigate= useNavigate()
-const handleSignUp = async(e)=>{
-    
-e.preventDefault()
-if(UserName?.trim()&&Password?.trim()&&Email?.trim()){
-    const user_data = {"UserName":UserName,"Email":Email,"Role":Role,"Password":Password,"Leader_id":"NaN"}
-    console.log(user_data)
-        const response = await fetch("http://localhost:4000/api/user/",{
-        method:"POST",
-        body:JSON.stringify(user_data),
-        headers:{
-          'Content-Type':'application/JSON'
+    const [UserName, setUserName] = useState("")
+    const [Password, setPassword] = useState("")
+    const [error, seterror] = useState("")
+    const [Email, setEmail] = useState("")
+    const [Role, setRole] = useState("Team Lead")
+    const navigate = useNavigate()
+    const handleSignUp = async (e) => {
+
+        e.preventDefault()
+        if (UserName?.trim() && Password?.trim() && Email?.trim()) {
+            const user_data = { "UserName": UserName, "Email": Email, "Role": Role, "Password": Password, "Leader_id": "NaN" }
+            console.log(user_data)
+            const response = await fetch("http://localhost:4000/api/user/", {
+                method: "POST",
+                body: JSON.stringify(user_data),
+                headers: {
+                    'Content-Type': 'application/JSON'
+                }
+            })
+            if (response.ok) {
+                navigate("/Login", { replace: true })
+            }
+        } else {
+            seterror("Something went wrong.")
         }
-      })
-      if(response.ok){
-      navigate("/Login",{replace:true})}
-}else{
-    seterror("Something went wrong.")
-}
-}
+    }
     return (
         <div className="w-full h-full p-10 md:px-[150px] flex">
             <div className="bg-[#dedede] md:w-[50%] h-full md:flex hidden flex-col justify-between">
@@ -38,30 +39,30 @@ if(UserName?.trim()&&Password?.trim()&&Email?.trim()){
                     <div className="flex flex-col gap-4">
                         <div className="flex flex-col text-start gap-1">
                             <span className="text-lg ml-4">Username</span>
-                            <input type="text" value={UserName} placeholder="Enter your username" name="username" id="" className=" p-4 rounded-[50px] border-[2px] border-[#dedede]" 
-                            onChange={(e)=>setUserName(e.target.value)}
+                            <input type="text" value={UserName} placeholder="Enter your username" name="username" id="" className=" p-4 rounded-[50px] border-[2px] border-[#dedede]"
+                                onChange={(e) => setUserName(e.target.value)}
                             />
                         </div>
                         <div className="flex flex-col text-start gap-1">
                             <span className="text-lg ml-4">Email</span>
                             <input type="email" value={Email} placeholder="Enter your email" name="email" id="" className=" p-4 rounded-[50px] border-[2px] border-[#dedede]"
-                            onChange={(e)=>setEmail(e.target.value)}
+                                onChange={(e) => setEmail(e.target.value)}
                             />
                         </div>
                         <div className="flex flex-col text-start gap-1">
                             <span className="text-lg ml-4">Password</span>
                             <div className="w-full">
                                 <input type="password" value={Password} placeholder="Enter your password" name="password" id="" className="w-full p-4 rounded-[50px] border-[2px] border-[#dedede]"
-                                 onChange={(e)=>setPassword(e.target.value)}
-                                 />
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
                                 <i className="fa-solid fa-eye absolute translate-x-[-45px] translate-y-[23px]"></i>
                             </div>
                         </div>
                         <div className="flex flex-col text-start gap-1">
                             <h1>Role:</h1>
-                        <select name="task[tm]" className="py-2 px-3 mt-2 md:mt-3 rounded-full bg-white text-black text-sm md:text-base">
-                                <option onClick={()=>setRole("Team Lead")}>Team Leader</option>
-                                <option onClick={()=>setRole("Team Member")}>Team Member</option>
+                            <select name="task[tm]" className="py-2 px-3 mt-2 md:mt-3 rounded-full bg-white text-black text-sm md:text-base">
+                                <option onClick={() => setRole("Team Lead")}>Team Leader</option>
+                                <option onClick={() => setRole("Team Member")}>Team Member</option>
                             </select>
                         </div>
                     </div>
@@ -74,7 +75,7 @@ if(UserName?.trim()&&Password?.trim()&&Email?.trim()){
                             <Link to="/Login" className="font-semibold">
                                 Login
                             </Link>
-                            {error&&<h2 className="text-red-500 text-lg">{error}</h2>}
+                            {error && <h2 className="text-red-500 text-lg">{error}</h2>}
                         </div>
                     </div>
                 </form>
